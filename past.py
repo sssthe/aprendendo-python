3daac1d1e31f20fd87a16a0415fb99d876388bcc
sssthe_ <sthefanycarolaine837@gmail.com>
2022-11-19 17:21:31 -0300

    aprendendo e praticando python

---
+++ past.py
@@ -0,0 +1,97 @@
+# int = 28
+# print (int)
+# print(type(28.5))
+# print(type('pda'))
+# int = inteiro 
+# float = numero flutuante
+# str = string 
+# list = lista (conjunto de dados mutaveis)
+# bool = boleano (razão lógica, verdadeiro ou falso)
+# tuple = tupla (conjunto de dados imutaveis)
+# dict = dicionario (conjunto de dados com chave e valor
+
+# 12.4.332.67
+
+# print (type(1238,58))
+
+# list('Morango, Pera, Manga e Carambola')
+# print(frutas)
+
+# print(['Morango, Pera, Manga e Carambol'])
+
+
+
+# dicionario 
+# altura = {'Tais': '1.78'}
+# print(altura) 
+
+
+
+# testes list
+
+# alunos = ['sthe', 'glessi', 'renan', 'josias']
+# notas = [9.0, 15, 9.9, 15] 
+# print(type(alunos))
+# print(type(notas))
+
+# celulares= ['samsung', 'motorola', 'iphone']
+# print(type(celulares))
+
+
+
+# teste dict 
+
+# altura = {'sthe':1.67, 'maria':1.55 }
+# print(type(altura))
+# anime = {'naruto': 800}
+# print(type(anime))
+
+
+
+# teste tuple
+
+# compras = ('arroz', 'feijao', 'cenoura')
+# valores = (1.99,2.50,5.00)
+# print(type(valores))
+
+
+
+# treino STR
+
+# nome= 'sthe'
+# profissão = 'rica'
+# print(type(nome))
+# print(type(profissão))
+
+# cor= 'azul'
+# tinta = 'verde'
+# print(type(cor))
+
+
+
+# treino bool 
+
+# eu_sou_rica = True
+# eu_falo_ingles = False
+# print(type(eu_sou_rica))
+
+# verão = True
+# chuva = False
+# print(type(verão))
+
+
+
+# teste int 
+
+# int= -25
+# print(type(int))
+
+# arroz = 44
+# print(type(arroz))
+
+
+
+ teste float
+
+ bts = 7/4
+ print(type(bts))
